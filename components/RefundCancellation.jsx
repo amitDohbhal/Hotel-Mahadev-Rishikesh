250699d0d83c62aeb457105fb238e6778f41763c
Akhil Maratha <akhilmaratha58@gmail.com>
2025-08-26 18:36:48 +0530

    updated the refund cancellation policy

--- components/RefundCancellation.jsx
+++ components/RefundCancellation.jsx
@@ -4,105 +4,108 @@ const RefundCancellation = () => {
   return (
     <section className="bg-[#fffaf3] py-10 px-4 md:px-12 w-full mx-auto rounded-lg shadow-sm">
       <div className="border border-black p-4 rounded-xl shadow-sm">
-        <h2 className="text-2xl md:text-3xl font-bold mb-6 text-start">Our Refund and Cancellation Policy</h2>
+        <h2 className="text-2xl md:text-3xl font-bold mb-6 text-start">Cancellation & Refund Policy</h2>
         <p className="mb-4">
-          <span className="font-semibold">http://www.hotelmahadevrishikesh.com (Website)</span> (“we”, “our”, “us”) A refund policy of an E-commerce platform states that if a consumer is unhappy with a product or service, they will return it and receive a refund for the amount paid at the time of purchase. In certain situations, the consumer has a time period beyond which they cannot return the product, such as 14 days or 40 days from the date of refund request.
+          Guests are requested to cancel reservation within the stipulated period as mentioned below to avoid
+          any cancellation charges.
         </p>
-        <div className="space-y-5">
-          <div>
-            <strong>1. Return policy :-</strong>
-            <p className="text-justify mt-1">
-              We have a customer friendly return policy. You will have to just write us on <a href="mailto:hotelmahadev.rishikesh@gmail.com" className="text-blue-600 underline font-semibold">hotelmahadev.rishikesh@gmail.com</a> within 24 hrs from time of delivery &amp; return steps will given from our customer support. <br />
-              1)- Provide us with the tracking details of the package or programme. Without this packages could get lost and any kind off replacement might be cancelled. <br />
-              Mention the Company Name http://www.hotelmahadevrishikesh.com (Website) and booking order request number ( --------------- ) on the return package. Last date of return refund request: 14 days from date of delivery. We will not be able to accept refund request after 15 days from date of booking process.
-            </p>
-          </div>
-          <div>
-            <strong>2. Cancelation Cost: :- You can choose to do one of the following</strong>
-            <p className="text-justify mt-1">
-              The cost of return shipping will not be covered by http://www.hotelmahadevrishikesh.com (Website) Please note that the below return policy is NOT applicable on orders that are custom made or third party or agents.<br /><strong>Options after return</strong><br />
-              1)- Request a replacement, either the same item or a different item from the website <br />
-              2)- Request a discount code of same value for purchase or booked later on the website. Using the code, you can select anything on the website upto the cancellations value.
-              3)- Request a refund. We will do a direct refund to the card through which payment was completed. In case the payment was done with cash, kindly provide us your bank account details on email <a href="mailto:hotelmahadev.rishikesh@gmail.com" className="text-blue-600 underline font-semibold">hotelmahadev.rishikesh@gmail.com</a>
-            </p>
-          </div>
-          <div>
-            <strong>3. What we do about mispricings :-</strong>
-            <p className="text-justify mt-1">
-              Despite our best efforts, a small number of the many thousands of content in our catalogue are mispriced. However, we verify prices as part of our procedures. If a package price is lower than our stated price, we charge the lower amount and send you the confirmation.
-            </p>
-          </div>
-          <div>
-            <strong>4. Cancellation of Fraudulent/Loss to business Orders :- </strong>
-            <p className="text-justify mt-1">
-              To provide a safe and secure booking experience, we regularly monitor transactions for fraudulent activity. In the event of detecting any suspicious activity, http://www.hotelmahadevrishikesh.com (Website) reserves the right to cancel all past, pending and future booking orders without any liability. http://www.hotelmahadevrishikesh.com (Website) also reserves the right to refuse or cancel booking orders in scenarios like inaccuracies in pricing of packages or Programme on website and unavailability. We may also require additional verifications or information before accepting any booking order. We will contact you if all or any portion of your booking order is cancelled or if additional information is required to accept your booking order. If your booking order is cancelled after your card has been charged, the said amount will be reversed to our Bank Account. Any promotional voucher used for the cancelled booking orders may not be refunded.
-            </p>
-          </div>
-          <div>
-            <strong>5. The customer may be considered fraudulent if any of the following scenarios are met :- </strong>
-            <p className="text-justify mt-1">
-              Customer doesn't reply to the payment verification mail sent by http://www.hotelmahadevrishikesh.com (Website)
-              <br />
-              Customer fails to produce adequate documents during the payment details verification
-              <br />
-              Misuse of another customer's phone/email
-              <br />
-              Customer uses invalid email and phone no.
-              <br />
-              Overuse of a voucher code
-              <br />
-              Customer refuses to pay for an order
-              <br />
-              Customer is involved in the snatch and run for any booking order.
-            </p>
-          </div>
-          <div>
-            <strong>6. Prior notification :-</strong>
-            <p className="text-justify mt-1">
-              Your are deemed to have accepted the terms and conditions. http://www.hotelmahadevrishikesh.com ( Website ) reserve right to time modify the Terms & Conditions of Use of the site without any prior notification to you.
-            </p>
-          </div>
-          <div>
-            <strong>7. Refund Policy :-</strong>
-            <p className="text-justify mt-1">
-              http://www.hotelmahadevrishikesh.com ( Website ) reserves the sole right to accept or deny refunds and will act reasonably in the common interest of both parties involved. Purchases (Booking Confirmation) are only retractable in exceptional circumstances, such as a clear typographical error is made, or there if a seller defaults in any way with the package listed or in the confirmations of the same.
-            </p>
-          </div>
-          <div>
-            <strong>8. No Guarantee :-</strong>
-            <p className="text-justify mt-1">
-              http://www.hotelmahadevrishikesh.com ( Website ) does not guarantee continuous, uninterrupted access to the Site, and operation of the Site may be interfered with by numerous factors outside http://www.hotelmahadevrishikesh.com ( Website ) control.
-            </p>
-          </div>
-          <div>
-            <strong>9. Disclaimer :-</strong>
-            <p className="text-justify mt-1">
-              We, our subsidiaries, officers, directors, employees and our suppliers provide the Site and the Services on an "as is" basis and without any warranty or condition, express, implied or statutory. We do not guarantee and do not promise any specific results from use of the Site and/or the Services. If any provision of this User Agreement is held to be invalid or unenforceable, such provision shall be struck out and the remaining provisions shall be enforceable. We reserve the right to alter these Terms & Conditions at any time. If the alterations constitute a material change to the Terms & Conditions , we will notify you via email according to the preference expressed on your account. What constitutes a "material change" will be determined at our sole discretion, in good faith and using common sense and reasonable judgement. You agree that this User Agreement and all incorporated agreements may be automatically assigned by http://www.hotelmahadevrishikesh.com ( Website ) in our sole discretion, to a third party in the event of a merger or acquisition or any other event. You and http://www.hotelmahadevrishikesh.com (Website) are independent contractors, and no agency, partnership, joint venture, employee-employer or franchisor-franchisee relationship is intended or created by this User Agreement. Headings are for reference purposes only and in no way define, limit, construe or describe the scope or extent of such section. Our failure to act with respect to a breach by you or others does not waive our right to act with respect to subsequent or similar breaches. This User Agreement sets forth the entire understanding and agreement between us with respect to its subject matter. No oral explanation or oral information given by any party shall alter the interpretation of this User Agreement. This Agreement will prevail over any conditions of sale imposed by a seller. Our Fees Policy is incorporated in this User Agreement by reference. The fees policy may be amended from time to time and material changes will be posted on the Site and sent to you at your registered email address.
-            </p>
-          </div>
-          <div>
-            <strong>10. Contact Information for Refund and cancellation Related Queries   :- </strong>
-            <p className="text-justify mt-1" style={{ textAlign: 'justify' }}>
-              “http://www.hotelmahadevrishikesh.com ( Website) ” is a registered trade mark.<br />
-              PAN Number :- AOPPA3234H  <br />
-              GST Registration Number :- 05AOPPA3234H3Z7                <br />
-              Phone Number : Call: +91 1354053504, +91 9557701203, +91 9927677716
-              <br />
-              Working Hours : 8:30 am - 18:30 pm
-              <br />
-              Email Id:
-              <a href="mailto:hotelmahadev.rishikesh@gmail.com"><strong><u> hotelmahadev.rishikesh@gmail.com </u></strong> </a><br />
 
-              Web: https://hotelmahadevrishikesh.com/
-              <br />
-              Hotel Address: NH-7, Rishikesh Delhi Highway
-              Above Reliance Store, Adjacent to Neem Karoli Temple Rishikesh 249203 (Uttarakhand)
+        <div className="space-y-6 text-justify">
+          {/* 1. Cancellation Policy */}
+          <div>
+            <strong>1. Cancellation Policy</strong>
+            <ul className="list-disc pl-6 mt-2 space-y-2">
+              <li><strong>Free Cancellation:</strong> Cancellations made 7 days prior to check-in are eligible for a full refund.</li>
+              <li><strong>Partial Refund:</strong> Cancellations made within 3–6 days prior to check-in will incur a 50% charge of the total booking amount.</li>
+              <li><strong>No Refund:</strong> Cancellations made less than 72 hours prior to check-in or in case of a no-show will not be eligible for a refund.</li>
+            </ul>
+            <ul className="list-disc pl-6 mt-3 space-y-1">
+              <li>No refunds for unused nights or early check-out.</li>
+              <li>No refunds for cancellation during peak season period.</li>
+              <li>No refund in case the booking is cancelled due to cancellation of flights/trains/ferries or road blocks due to climatic changes/natural calamities/political disturbances, etc.</li>
+              <li>The cancellation policy for some rates/offers and room category may differ from the standard cancellation policy of the hotel.</li>
+            </ul>
+          </div>
+
+          {/* 2. Arrival and Departure Policy */}
+          <div>
+            <strong>2. Arrival and Departure Policy</strong>
+            <p className="mt-2">
+              A recent Government notification requires guests to present proof of identity at the time of check in.
+              Guests are requested to carry with them the required document during their travel.
+            </p>
+            <p className="mt-5">
+              <strong>Early Arrivals and Late Departures:</strong>  
+              <p className="mt-2">
+                If you expect to arrive early in the day and would like immediate access to your room, we recommend booking the room for the prior night to guarantee immediate access. Similarly, for late departures, reserving an additional night will guarantee access to your guest room until you depart. If you choose not to reserve, we will be glad to store your luggage while you relax in the lobby.
+              </p>
+            </p>
+          </div>
+
+          {/* 3. Guarantee Policy */}
+          <div>
+            <strong>3. Guarantee Policy</strong>
+            <ul className="list-disc pl-6 mt-2 space-y-2">
+              <li>All bookings must be guaranteed at time of reservation by Credit Card or Travel Agency. All major credit cards are accepted.</li>
+              <li>At Hotel Mahadev Rishikesh, a guaranteed reservation assures you of a room even if you check in late (after 6:00 pm), on receipt of advance payment. All reservations made through the website must be guaranteed by a major credit card. Deposits are required for confirmed bookings only. Please check for full deposit requirements at time of booking.</li>
+            </ul>
+          </div>
+
+          {/* 4. Credit Cards */}
+          <div>
+            <strong>4. Credit Cards</strong>
+            <ul className="list-disc pl-6 mt-2">
+              <li>Visa and MasterCard</li>
+            </ul>
+          </div>
+
+          {/* 5. Child Policy */}
+          <div>
+            <strong>5. Child Policy</strong>
+            <p className="mt-2">
+              Hotel Mahadev Rishikesh charges no additional fee for children 5 years of age and younger occupying the same room as their parents or guardians (space permitting). Special rates may be available for children between 6–12 years occupying the same room as their parents or guardians. Meals for children can be availed on extra charges.
+            </p>
+          </div>
+
+          {/* 6. Banking Policy */}
+          <div>
+            <strong>6. Banking Policy</strong>
+            <p className="mt-2">
+              We as a merchant shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring Bank from time to time.
+            </p>
+          </div>
+
+          {/* 7. Disclaimer – Hotel Mahadev */}
+          <div>
+            <strong>7. Disclaimer – Hotel Mahadev</strong>
+            <ul className="list-disc pl-6 mt-2 space-y-2">
+              <li>All bookings at Hotel Mahadev are subject to availability and confirmation.</li>
+              <li>The hotel reserves the right to modify, amend, or update the cancellation and refund policy without prior notice.</li>
+              <li>Guests are responsible for providing accurate contact details at the time of booking. The hotel will not be held liable for any communication failure due to incorrect information.</li>
+              <li>Refunds, where applicable, are processed only through the original payment method used during booking. Transaction or processing fees (if any) charged by banks/payment gateways are non-refundable.</li>
+              <li>The hotel shall not be held responsible for cancellations arising due to circumstances beyond its control, including but not limited to natural disasters, pandemics, government restrictions, or transportation delays. In such cases, alternate solutions such as date changes or credit vouchers may be offered at the hotel’s discretion.</li>
+              <li>By confirming a booking, guests acknowledge and agree to abide by the hotel’s policies, house rules, and terms of stay.</li>
+            </ul>
+          </div>
+
+          {/* Contact Information */}
+          <div>
+            <strong>Contact Information for Refund and Cancellation Related Queries:</strong>
+            <p className="mt-2">
+              “http://www.hotelmahadevrishikesh.com (Website)” is a registered trade mark.<br />
+              <strong>PAN Number:</strong> AOPPA3234H <br />
+              <strong>GST Registration Number:</strong> 05AOPPA3234H3Z7 <br />
+              <strong>Phone Number:</strong> +91 1354053504, +91 9557701203, +91 9927677716 <br />
+              <strong>Working Hours:</strong> 8:30 am - 18:30 pm <br />
+              <strong>Email:</strong> <a href="mailto:hotelmahadev.rishikesh@gmail.com" className="text-blue-600 underline font-semibold">hotelmahadev.rishikesh@gmail.com</a> <br />
+              <strong>Web:</strong> <a href="https://hotelmahadevrishikesh.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline font-semibold">https://hotelmahadevrishikesh.com</a> <br />
+              <strong>Hotel Address:</strong> NH-7, Rishikesh Delhi Highway, Above Reliance Store, Adjacent to Neem Karoli Temple, Rishikesh 249203 (Uttarakhand)
             </p>
           </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
 
 export default RefundCancellation
